0397d85740e9486b61d4a33f41e70d9a216795b7
Briana Craig <brianascraig@gmail.com>
2018-11-26 22:20:57 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 'use strict';
 
-function getCalendarDisplay () {
-  $('.viewMealCalendarButton').on('click', event => {
-   $('#calendar').fullCalendar({
-  defaultView: 'month'
-}); 
-  })
-}
 
-getCalendarDisplay();
