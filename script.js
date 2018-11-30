47e616263ea7702ab5cee85b6fe8c1ff1f538c98
Briana Craig <brianascraig@gmail.com>
2018-11-30 11:45:07 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
+function getHomeScreen () {
+  
+}
+
+function getFoodCategories() {
+  let baseUrl = "https://www.themealdb.com/api/json/v1/1/"
+  let foodCategoriesApi = "${baseUrl}/
+}
 
