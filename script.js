771c2fa6da54b662711f4c5581e7d3580c332fdb
Briana Craig <brianascraig@gmail.com>
2018-12-13 18:21:39 -0500

    updated index.html and script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function getExistingMealCalendar() {
     $('.js-homeScreen').toggleClass('hidden');
     $('.js-backButton').toggleClass('hidden');
     if (currentMealCalendar.length == 0) {
-      $('.js-calendarErrorMessage').html('You have not created a meal calendar yet.');
+      alert("You have not created a meal calendar yet. Please create a meal calendar first.")
     }
     else {
       getMealCalendarResults();
@@ -36,39 +36,58 @@ function backToHome() {
 function getMealCalendarResults() {
   //This function will display the latest meal calendar with the most
   //recent food preferences.
+  $('.foodPrefForm').submit(function(event){
+    event.preventDefault();
+    $('.js-foodPrefScreen').toggleClass('hidden');
+    $('.js-mealCalendarScreen').toggleClass('hidden');
+    $('.js-backButton').toggleClass('hidden');
+  });
 }
 
 function getFoodPrefScreen() {
   //This function will display the food preferences screen.
     $('.js-homeScreen').toggleClass('hidden');
-    $('.js-foodPrefScreen').toggleClass('.hidden');
+    $('.js-foodPrefScreen').toggleClass('hidden');
 };
+
+function updatePreviousMealCalendar() {
+  previousMealCalendar.length= 0;
+  previousMealCalendar = currentMealCalendar.slice();
+  currentMealCalendar.length = 0;
+}
 
 function changeMealCalendar() {
   $('.js-changeMealCalendarButton').on('click', function (event){
     event.preventDefault();
-    // getFoodPrefScreen();
-    $('.js-homeScreen').toggleClass('hidden');
-    $('.js-foodPrefScreen').toggleClass('.hidden');
-    $('.js-backButton').toggleClass('hidden');
-    previousMealCalendar.length= 0;
-    previousMealCalendar = currentMealCalendar.slice();
-    currentMealCalendar.length = 0;
+    getFoodPrefScreen();
+    console.log("changecalenda working");
+    updatePreviousMealCalendar();
+  });
+}
+
+function startNewMealCalendar() {
+  $('.js-newMealCalendar').on('click', function (event){
+    event.preventDefault();
+    getFoodPrefScreen();
+    updatePreviousMealCalendar();
   });
 }
 
 
 
-// function getFoodCategories() {
-//   let baseUrl = "https://www.themealdb.com/api/json/v1/1/";
-//   let foodCategoriesApi = "${baseUrl}";
-// }
+function getFoodCategories() {
+  let baseUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+  let categorySelection = input.val();
+  let foodCategoriesApi = '${baseUrl}${categorySelection}"';
+}
 
 function handleApp() {
   getHomeScreen();
   getExistingMealCalendar();
   changeMealCalendar();
   backToHome();
+  getMealCalendarResults();
+  startNewMealCalendar();
 }
 
 $(handleApp);
